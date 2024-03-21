d0132e07dca76475aba2fbafd04c148472a7f421
UnverOnal <onalunver@gmail.com>
2024-03-21 13:38:32 +0300

    Create scenes & some common services
    Services : Input, Scene, Pooling

---
+++ Assets/Scripts/Scopes/GameScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Scopes
+{
+    public class GameScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Scopes/MainScope.cs
@@ -0,0 +1,30 @@
+using Services.InputService;
+using Services.PoolingService;
+using Services.SceneService;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Scopes
+{
+    public class MainScope : LifetimeScope
+    {
+        [SerializeField] private SceneDataContainer sceneDataContainer;
+        
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterInstance(sceneDataContainer);
+            
+            InstallServices(builder);
+        }
+
+        private void InstallServices(IContainerBuilder builder)
+        {
+            builder.Register<ISceneService, SceneService>(Lifetime.Singleton);
+            builder.Register<IPoolService, PoolService>(Lifetime.Singleton);
+            
+            builder.Register<InputService>(Lifetime.Singleton).As<IInputService>().AsSelf();
+            builder.RegisterEntryPoint<InputEntryPoint>();
+        }
+    }
+}
